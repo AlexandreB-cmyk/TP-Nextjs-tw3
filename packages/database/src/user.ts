945ed7df9f91cb4ba5a1464a2e971eddab0bb923
micmc422 <micmc422@gmail.com>
2025-12-06 01:27:36 +0100

    Correction de l'extension de fichier pour l'importation du client MongoDB

--- packages/database/src/user.ts
+++ packages/database/src/user.ts
@@ -12,7 +12,7 @@
  */
 
 import { ObjectId, WithId } from 'mongodb';
-import { getCollection } from './mongodb-client.js';
+import { getCollection } from './mongodb-client.ts';
 
 /**
  * Interface représentant un utilisateur dans la base de données
